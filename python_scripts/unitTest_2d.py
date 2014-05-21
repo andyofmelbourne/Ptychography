72da2f1d7a390351f3a6466247235653d023fa1d
andyofmelbourne <andyofmelbourne@gmail.com>
2014-05-21 22:14:16 +0200

    unitTest_2d.py to test the 2d Ptychography routines

--- python_scripts/unitTest_2d.py
+++ python_scripts/unitTest_2d.py
@@ -1,8 +1,8 @@
 import numpy as np
 import scipy as sp
 from scipy import ndimage
-import sys
-import os
+import sys, os, getopt
+import subprocess 
 from ctypes import *
 import bagOfns as bg
 
@@ -13,6 +13,20 @@ import bagOfns as bg
 # output zero pixel
 # output probe coordinates
 
+def main(argv):
+    outputdir = './'
+    try :
+        opts, args = getopt.getopt(argv,"ho:",["outputdir="])
+    except getopt.GetoptError:
+        print 'python makeUnitTest.py -o <outputdir>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'python makeUnitTest.py -o <outputdir>'
+            sys.exit()
+        elif opt in ("-o", "--outputdir"):
+            outputdir = arg
+    return outputdir
 
 # Make a sample on a large grid
 shape_sample = (64, 128)
@@ -22,7 +36,7 @@ sample = amp * np.exp(1J * phase)
 
 # Make an illumination on the data grid
 shape_illum = (32, 64)
-probe       = bg.circle(shape_illum, radius=0.5, origin=[shape_illum[0]/2-1, shape_illum[1]/2 - 1]) + 0J
+probe       = bg.circle_new(shape_illum, radius=0.5, origin=[shape_illum[0]/2-1, shape_illum[1]/2 - 1]) + 0J
 
 # Make sample coordinates (y, x)
 # these will be relative shift coordinates in pixel units
@@ -52,7 +66,7 @@ def makeExit(sample, probe, shift = [0,0]):
     return exit
 
 # Make a detector mask
-# mask = np.array(bg.circle(shape_illum, radius=0.1), dtype=np.bool)
+# mask = np.array(bg.circle_new(shape_illum, radius=0.1), dtype=np.bool)
 # mask = ~mask
 # Just ones for now
 mask = np.ones_like(probe, dtype=np.bool)
@@ -64,30 +78,41 @@ for coord in coords:
     exitF = bg.fft2(makeExit(sample, probe, coord))
     diffs.append(np.abs(exitF)**2)
 
-def makeHeatMap(sample, probe, coords):
-    print 'making heatMap'
-    heatMap      = np.zeros_like(sample, dtype=np.float64)
-    sample_shift = np.zeros_like(sample, dtype=np.float64)
-    for coord in coords:
-        print 'processing coordinate', coord
-        sample_shift[:probe.shape[0], :probe.shape[1]] = np.abs(probe)
-        heatMap     += bg.roll(sample_shift, [-coord[0], -coord[1]])
-    return heatMap
-
 sampleInit = np.random.random((shape_sample)) + 1J*np.random.random((shape_sample))
 #sampleInit = sample
 #probeInit = np.random.random((shape_illum)) + 1J*np.random.random((shape_illum))
-#probeInit  = bg.circle(shape_illum, radius=0.3, origin=[shape_illum[0]/2-1, shape_illum[1]/2 - 1]) + 0J
+#probeInit  = bg.circle_new(shape_illum, radius=0.3, origin=[shape_illum[0]/2-1, shape_illum[1]/2 - 1]) + 0J
 probeInit  = probe
 
 # Output 
-dir_out = './test_cases/unit_test_Ptychography/'
-bg.binary_out(probe, dir_out + 'probe', dt=np.complex128, appendDim=True)
-bg.binary_out(sample, dir_out + 'sample', dt=np.complex128, appendDim=True)
-bg.binary_out(mask, dir_out + 'mask', dt=np.float64, appendDim=True)
-bg.binary_out(np.array(diffs), dir_out + 'diffs', dt=np.float64, appendDim=True)
-bg.binary_out(np.array(coords), dir_out + 'coords', dt=np.float64, appendDim=True)
+outputdir = main(sys.argv[1:])
+print 'outputputing files...'
+print 'output directory is ', outputdir
+
+sequence = """# This is a sequence file which determines the ptychography algorithm to use
+Thibault_sample = 200
+ERA_sample = 500
+"""
+
+with open(outputdir + "sequence.txt", "w") as text_file:
+    text_file.write(sequence)
+
+bg.binary_out(probe, outputdir + 'probe', dt=np.complex128, appendDim=True)
+bg.binary_out(sample, outputdir + 'sample', dt=np.complex128, appendDim=True)
+bg.binary_out(mask, outputdir + 'mask', dt=np.float64, appendDim=True)
+bg.binary_out(np.array(diffs), outputdir + 'diffs', dt=np.float64, appendDim=True)
+bg.binary_out(np.array(coords), outputdir + 'coords', dt=np.float64, appendDim=True)
+
+bg.binary_out(probeInit, outputdir + 'probeInit', dt=np.complex128, appendDim=True)
+bg.binary_out(sampleInit, outputdir + 'sampleInit', dt=np.complex128, appendDim=True)
 
 
-bg.binary_out(probeInit, dir_out + 'probeInit', dt=np.complex128, appendDim=True)
-bg.binary_out(sampleInit, dir_out + 'sampleInit', dt=np.complex128, appendDim=True)
+
+print 'Finished!'
+print ''
+print 'Now run the test with:'
+print 'python Ptychography.py -i', outputdir, ' -o',outputdir
+
+
+# send the job over ssh
+subprocess.Popen([sys.executable, 'Ptychography.py', '-i', outputdir, '-o', outputdir])
