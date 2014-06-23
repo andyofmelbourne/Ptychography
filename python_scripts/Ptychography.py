Fixed masking

python_scripts/Ptychography.py
@@ -485,7 +485,9 @@ class Ptychography_gpu(object):
         fft               = FFT(self.diffAmps_gpu.astype(np.complex128), axes=(1,2))
         self.fftc         = fft.compile(self.thr, fast_math=True)
         self.exits_gpu    = self.thr.to_device(self.exits)
-        self.mask_gpu     = self.thr.to_device(self.mask)
+        mask2             = np.zeros_like(diffs, dtype=np.complex128)
+        mask2[:]          = self.mask.astype(np.complex128)
+        self.mask_gpu     = self.thr.to_device(mask2)
 
     def ERA_sample(self, iters=1):
         exits2_gpu = self.thr.empty_like(self.exits_gpu)
@@ -508,7 +510,7 @@ class Ptychography_gpu(object):
         exits2_gpu = self.thr.empty_like(exits_gpu)
         self.fftc(exits2_gpu, exits_gpu)
         #
-        exits2_gpu    = exits2_gpu * (self.mask_gpu * self.diffAmps_gpu / (abs(exits2_gpu) + self.alpha_div) + ~self.mask_gpu)
+        exits2_gpu    = exits2_gpu * (self.mask_gpu * self.diffAmps_gpu / (abs(exits2_gpu) + self.alpha_div) + (1.0 - self.mask_gpu))
         #
         self.fftc(exits2_gpu, exits2_gpu, True)
         return exits2_gpu

python_scripts/unitTest_1dsample.py
@@ -29,7 +29,7 @@ def main(argv):
     return outputdir
 
 # Make a sample on a large grid
-shape_sample = (256, 7258)
+shape_sample = (128, 7258)
 amp          = bg.scale(bg.brog(shape_sample), 0.0, 1.0)
 phase        = bg.scale(bg.twain(shape_sample), -np.pi, np.pi)
 sample       = amp * np.exp(1J * phase)
@@ -41,7 +41,7 @@ sample_1d = sample[sample.shape[0]/2, :]
 sample[:] = sample_1d
 
 # Make an illumination on the data grid
-shape_illum = (256, 2982)
+shape_illum = (128, 2982)
 probe       = bg.circle_new(shape_illum, radius=0.5, origin=[shape_illum[0]/2-1, shape_illum[1]/2 - 1]) + 0J
 
 # Make sample coordinates (y, x)
@@ -74,10 +74,10 @@ def makeExit(sample, probe, shift = [0,0]):
     return exit
 
 # Make a detector mask
-mask = np.array(bg.circle_new(shape_illum, radius=0.05), dtype=np.bool)
+mask = np.array(bg.circle_new(shape_illum, radius=0.02), dtype=np.bool)
 # mask = ~mask
 # Just ones for now
-#mask = np.ones_like(probe, dtype=np.bool)
+mask = np.ones_like(probe, dtype=np.bool)
 
 print 'making diffraction patterns'
 diffs = []