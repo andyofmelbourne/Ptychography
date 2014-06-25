39fceb4970afb60fcd04fb408787b3115fc553e0
Andrew Morgan <andrew.morgan@desy.de>
2014-06-25 14:53:12 +0200

    Add shift and complex constant invariant error metric (fidelity)

--- python_scripts/unitTest_2d.py
+++ python_scripts/unitTest_2d.py
@@ -29,13 +29,13 @@ def main(argv):
     return outputdir
 
 # Make a sample on a large grid
-shape_sample = (256, 512)
+shape_sample = (128, 256)
 amp          = bg.scale(bg.brog(shape_sample), 0.0, 1.0)
 phase        = bg.scale(bg.twain(shape_sample), -np.pi, np.pi)
 sample       = amp * np.exp(1J * phase)
 
 # Make an illumination on the data grid
-shape_illum = (128, 256)
+shape_illum = (64, 128)
 probe       = bg.circle_new(shape_illum, radius=0.5, origin=[shape_illum[0]/2-1, shape_illum[1]/2 - 1]) + 0J
 
 # Make sample coordinates (y, x)
@@ -44,7 +44,7 @@ probe       = bg.circle_new(shape_illum, radius=0.5, origin=[shape_illum[0]/2-1,
 # so sample_shifted = sample(y - yi, x - xi)
 # These will be a list of [y, x]
 
-dx = dy = 10
+dx = dy = 5
 x, y    = np.meshgrid(  range(3, shape_sample[1] - probe.shape[1] - 3, dx), range(3, shape_sample[0] - probe.shape[0] - 3, dy))
 coords0 = zip(y.flatten(), x.flatten())
 coords0 = -np.array(coords0)
@@ -86,11 +86,11 @@ for coord in coords:
     temp = bg.roll(temp, -coord)
     heatmap += np.abs(temp)**2
 
-#sampleInit = np.random.random((shape_sample)) + 1J*np.random.random((shape_sample))
-sampleInit = sample
-probeInit = np.random.random((shape_illum)) + 1J*np.random.random((shape_illum))
+sampleInit = np.random.random((shape_sample)) + 1J*np.random.random((shape_sample))
+#sampleInit = sample
+#probeInit = np.random.random((shape_illum)) + 1J*np.random.random((shape_illum))
 #probeInit  = bg.circle_new(shape_illum, radius=0.3, origin=[shape_illum[0]/2-1, shape_illum[1]/2 - 1]) + 0J
-#probeInit  = probe
+probeInit  = probe
 
 # Output 
 outputdir = main(sys.argv[1:])
@@ -98,7 +98,7 @@ print 'outputputing files...'
 print 'output directory is ', outputdir
 
 sequence = """# This is a sequence file which determines the ptychography algorithm to use
-ERA_probe = 100
+ERA_both = 1000
 """
 
 with open(outputdir + "sequence.txt", "w") as text_file:
@@ -124,20 +124,44 @@ print 'python Ptychography.py -i', outputdir, ' -o',outputdir
 # run Ptychography and time it using the bash command "time"
 subprocess.call('time python Ptychography.py' + ' -i' + outputdir + ' -o' + outputdir, shell=True)
 
+def fidelity(o1 , o2):
+    return np.sqrt(np.sum(np.abs(o1 - o2)**2) / np.sum(np.abs(o1)**2))
+
+def fidelity_cons(o1 , o2):
+    c = np.sum(np.conj(o1) * o2) / np.sum(np.abs(o1)**2)
+    return fidelity(c * o1, o2)
+
+def fidelity_shift(o1 , o2):
+    errors = []
+    for i in range(o1.shape[0]):
+        for j in range(o1.shape[1]):
+            o_shift = bg.roll(o1, [i, j])
+            errors.append(fidelity(o_shift, o2))
+    return np.array(errors).min()
+
+def fidelity_shift_cons(o1 , o2):
+    errors = []
+    for i in range(o1.shape[0]):
+        for j in range(o1.shape[1]):
+            o_shift = bg.roll(o1, [i, j])
+            errors.append(fidelity_cons(o_shift, o2))
+    return np.array(errors).min()
+
+def fidelity_shift_cons_mask(o1, masko1, o2):
+    errors = []
+    for i in range(o1.shape[0]):
+        for j in range(o1.shape[1]):
+            o_shift = bg.roll(o1, [i, j])
+            m_shift = bg.roll(masko1, [i, j])
+            errors.append(fidelity_cons(o_shift, o2 * m_shift)) 
+    return np.array(errors).min()
+
+
 sample_ret = bg.binary_in(outputdir + 'sample_retrieved', dt=np.complex128, dimFnam=True)
-c = np.sum(np.conj(sample_ret) * sample) / np.sum(np.abs(sample_ret)**2 + 1.0e-10)
-print 'sample error', bg.l2norm(c * sample_ret, sample)
+probe_ret  = bg.binary_in(outputdir + 'probe_retrieved', dt=np.complex128, dimFnam=True)
 
 mask = (heatmap > 1.0e-1 * heatmap.max())
 print 'mask area' , np.sum(mask)
 
-sample_ret_m = sample_ret * mask
-sample_m = sample * mask
-c = np.sum(np.conj(sample_ret_m) * sample_m) / np.sum(np.abs(sample_ret_m)**2 + 1.0e-10)
-print 'sample error', bg.l2norm(c * sample_ret_m, sample_m)
-
-
-
-probe_ret = bg.binary_in(outputdir + 'probe_retrieved', dt=np.complex128, dimFnam=True)
-c = np.sum(np.conj(probe_ret) * probe) / np.sum(np.abs(probe_ret)**2 + 1.0e-10)
-print 'probe error', bg.l2norm(c * probe_ret, probe)
+print 'sample error shift and constant corrected:', fidelity_shift_cons_mask(sample, mask, sample_ret)
+print 'probe  error shift and constant corrected:', fidelity_shift_cons(probe, probe_ret)
