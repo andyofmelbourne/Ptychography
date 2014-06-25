53ce4900e2df95a1d064f6dfcb6905c97c063c81
Andrew Morgan <andrew.morgan@desy.de>
2014-06-25 19:36:58 +0200

    Modify pipline.py for it-hpc-gpu06

--- MLL_analysis/pipeline.py
+++ MLL_analysis/pipeline.py
@@ -18,8 +18,7 @@ import subprocess
 ##########################################
 ##########################################
 sequence = """
-pmod_int = True
-Huang = 1
+ERA_both = 10
 """
 
 gratingSim = False
@@ -83,6 +82,51 @@ if __name__=='__main__':
         print 'I hope all went well!'
         print 'Your results should be in ' + tempdata_dir 
         #
+    elif loc == 'process':
+        commands = []
+        #
+        # process the .h5 files
+        commands.append('python process_diffs.py --scan=0181 --run='+str(run)+' --outputdir=' + tempdata_dir + ' --samplesupport='+str(samplesupport)+' --sample1d='+str(sample1d)+' --gratingSim='+str(gratingSim))
+        #
+        # Output the algorithm parameters
+        print '#########################################################'
+        print 'Outputing the sequence.txt file...'
+        with open(tempdata_dir + "sequence.txt", "w") as text_file:
+            text_file.write(sequence)
+        print '#########################################################'
+        #
+        # execute sequentially
+        for command in commands:
+            print command
+            try :
+                retcode = subprocess.call(command, shell=True)
+                if retcode < 0:
+                    print >>sys.stderr, "Child was terminated by signal", -retcode
+            except OSError as e:
+                print >>sys.stderr, "Execution failed:", e
+        #
+    elif loc == 'run':
+        commands = []
+        #
+        # Run the retrieval on the data 
+        commands.append('python ../python_scripts/Ptychography.py -i ' +tempdata_dir+ ' -o ' +tempdata_dir)
+        #
+        # Process the results 
+        commands.append('python process_results.py -i ' +tempdata_dir+ ' -o ' +tempdata_dir)
+        #
+        # execute sequentially
+        for command in commands:
+            print command
+            try :
+                retcode = subprocess.call(command, shell=True)
+                if retcode < 0:
+                    print >>sys.stderr, "Child was terminated by signal", -retcode
+            except OSError as e:
+                print >>sys.stderr, "Execution failed:", e
+        #
+        print 'I hope all went well!'
+        print 'Your results should be in ' + tempdata_dir 
+        #
     elif loc == 'cfelsgi':
         # send ../Ptychography to cfelsgi 
         # then run this script "locally" on cfelsgi by remotely sending the command over ssh
@@ -98,6 +142,35 @@ if __name__=='__main__':
         #
         # retrieve the data
         commands.append('rsync -qe ssh --recursive --progress amorgan@cfelsgi:/home/amorgan/tempdata/ ../../../tempdata/')
+        #
+        # execute sequentially
+        for command in commands:
+            print command
+            try :
+                retcode = subprocess.call(command, shell=True)
+                if retcode < 0:
+                    print >>sys.stderr, "Child was terminated by signal", -retcode
+            except OSError as e:
+                print >>sys.stderr, "Execution failed:", e
+        # 
+    elif loc == 'it-hpc-gpu06':
+        # send ../Ptychography to cfelsgi 
+        # then run this script "locally" on cfelsgi by remotely sending the command over ssh
+        # then call in tempdata from cfelsgi to here
+        # This only works if the relative directory trees are the same
+        commands = []
+        #
+        # send the job over ssh
+        commands.append('rsync -e ssh --recursive --progress --delete ../../Ptychography/ amorgan@it-hpc-gpu06:/nfs/cfel/cxi/home/amorgan/analysis/Ptychography')
+        #
+        # process the results using cfelsgi (which can access the data)
+        commands.append("ssh cfelsgi 'export LD_LIBRARY_PATH='/cfel/common/lib:$LD_LIBRARY_PATH'; export PYTHONPATH='/nfs/cfel/cxi/common/cfelsgi/gcc_4_4_7/python-hdf5/2.3.0/lib64/python2.6/site-packages:$PYTHONPATH'; cd /home/amorgan/analysis/Ptychography/MLL_analysis/; python pipeline.py --location=process --run="+str(run)   +" --tempdata_dir="+tempdata_dir   +" ' ")
+        #
+        # run the results 
+        commands.append("ssh it-hpc-gpu06 'eval $(./export_python.sh); cd /nfs/cfel/cxi/home/amorgan/analysis/Ptychography/MLL_analysis/; python pipeline.py --location=run --run="+str(run)   +" --tempdata_dir="+tempdata_dir   +" ' ")
+        #
+        # retrieve the data
+        commands.append('rsync -e ssh --recursive --progress amorgan@it-hpc-gpu06:/nfs/cfel/cxi/home/amorgan/tempdata/ ../../../tempdata/')
         #
         # execute sequentially
         for command in commands:
