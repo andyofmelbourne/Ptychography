c5ef5b42976b48cfe7a8797cad002294b581e096
Andrew Morgan <andyofmelbourne@gmail.com>
2016-02-16 10:54:06 +0100

    adding top level __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+#import bagOfns
+from ptychography.era import ERA
+from ptychography.dm import DM
+from ptychography.back_projection import Back_projection
--- ptychography/__init__.py
+++ ptychography/__init__.py
@@ -1,6 +1,3 @@
-#import bagOfns
 import era
 import dm
-from era import ERA
-from dm import DM
-from back_projection import Back_projection 
+import back_projection
