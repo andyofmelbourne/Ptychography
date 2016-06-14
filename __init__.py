9d1ef09a2bee67f6543849ee5d61c634aca28519
Andrew Morgan <andyofmelbourne@gmail.com>
2016-06-14 11:30:20 +0200

    main module imports utils

--- __init__.py
+++ __init__.py
@@ -6,3 +6,4 @@ from ptychography.back_projection import Back_projection
 from ptychography.forward_models import forward_sim
 from ptychography.display import write_cxi
 from ptychography.display import display_cxi
+from ptychography import utils
--- test.py
+++ test.py
@@ -30,7 +30,7 @@ else :
 if rank == 0 :
     print '\nMaking the forward simulated data...'
     I, R, M, P, O, B = forward_sim(shape_P = (128, 128), shape_O = (256, 256), A = 32, defocus = 1.0e-2,\
-				      photons_pupil = 1, ny = 10, nx = 10, random_offset = 5, \
+				      photons_pupil = 1, ny = 20, nx = 20, random_offset = 5, \
 				      background = None, mask = 100, counting_statistics = False)
     # make the masked pixels bad
     I += 10000. * ~M 
